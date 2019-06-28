ea8f362157c062226b8df9b16ea6c26a58e5d2d6
Administrator <Administrator@A36Y7DGX605K5WC>
2019-06-28 10:11:26 +0800

    1

---
+++ sp01-commons/src/main/java/com/tedu/sp01/pojo/Item.java
@@ -0,0 +1,19 @@
+package com.tedu.sp01.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Item {
+	//id
+	private Integer id;
+	//Ãû³Æ
+	private String name;
+	private Integer number;
+	
+	//1111111111
+	//33333333
+}
